6a2bd2396fb95d0bf50771a15f2800684c72e53f
Muhammet Baki <mubaki61n@gmail.com>
2023-11-08 22:01:30 +0300

    d02

--- src/main/java/deneme01/Deneme01.java
+++ src/main/java/deneme01/Deneme01.java
@@ -3,5 +3,6 @@ package deneme01;
 public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello git hub");
     }
 }
